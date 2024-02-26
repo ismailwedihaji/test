af16d773a984da56171505e039dead63af9d64a7
ismailwedihaji <ismailwedihaji@gmail.com>
2024-02-26 12:33:25 +0100

    try new

--- client/src/App.js
+++ client/src/App.js
@@ -1,82 +1,57 @@
-// import './App.css';
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// import LoginPresenter from './presenter/LoginPresenter';
-// import RegisterPresenter from './presenter/RegisterPresenter';
-// import ApplicationPresenter from './presenter/ApplicationPresenter';
-// import ApplicationsListPresenter from './presenter/ApplicationsListPresenter';
-// import Dashboard from './view/Dashboard';
-// import NotAuthorized from './view/NotAuthorized';
-// import ProtectedRoute from './components/ProtectedRoute';
-// import Layout from './components/Layout';
-
-// function App() {
-
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Routes>
-//           <Route path="/" element={<Navigate replace to="/login" />} />
-//           <Route path="/login" element={
-//             <Layout showHeader={false}>
-//               <LoginPresenter />
-//             </Layout>
-//           } />
-//           <Route path="/register" element={
-//             <Layout showHeader={false}>
-//               <RegisterPresenter />
-//             </Layout>
-//           } />
-//           <Route path="/dashboard" element={
-//             <Layout showHeader={true}>
-//               <ProtectedRoute><Dashboard /></ProtectedRoute>
-//             </Layout>
-//           } />
-//           <Route path="/apply" element={
-//             <Layout showHeader={true}>
-//               <ProtectedRoute><ApplicationPresenter /></ProtectedRoute>
-//             </Layout>
-//           } />
-//           <Route path="/applications" element={
-//             <Layout showHeader={true}>
-//               <ProtectedRoute allowedRoles={[1]}>
-//                 <ApplicationsListPresenter />
-//               </ProtectedRoute>
-//             </Layout>
-//           } />
-//           <Route path="/not-authorized" element={
-//             <Layout showHeader={true}>
-//               <NotAuthorized />
-//             </Layout>
-//           } />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-import {useEffect} from 'react';
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPresenter from './presenter/LoginPresenter';
+import RegisterPresenter from './presenter/RegisterPresenter';
+import ApplicationPresenter from './presenter/ApplicationPresenter';
+import ApplicationsListPresenter from './presenter/ApplicationsListPresenter';
+import Dashboard from './view/Dashboard';
+import NotAuthorized from './view/NotAuthorized';
+import ProtectedRoute from './components/ProtectedRoute';
+import Layout from './components/Layout';
 
 function App() {
-  const makeAPICall = async () => {
-    try {
-      const response = await fetch('https://myapp-backend-ys93.onrender.com', {mode: 'cors'});
-      const data = await response.json();
-      console.log({data});
-    } catch (e) {
-      console.log(e);
-    }
-  };
 
-  useEffect(() => {
-    makeAPICall();
-  }, []);
   return (
-    <div className="App">
-      <h1>Called server on other domain</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/login" />} />
+          <Route path="/login" element={
+            <Layout showHeader={false}>
+              <LoginPresenter />
+            </Layout>
+          } />
+          <Route path="/register" element={
+            <Layout showHeader={false}>
+              <RegisterPresenter />
+            </Layout>
+          } />
+          <Route path="/dashboard" element={
+            <Layout showHeader={true}>
+              <ProtectedRoute><Dashboard /></ProtectedRoute>
+            </Layout>
+          } />
+          <Route path="/apply" element={
+            <Layout showHeader={true}>
+              <ProtectedRoute><ApplicationPresenter /></ProtectedRoute>
+            </Layout>
+          } />
+          <Route path="/applications" element={
+            <Layout showHeader={true}>
+              <ProtectedRoute allowedRoles={[1]}>
+                <ApplicationsListPresenter />
+              </ProtectedRoute>
+            </Layout>
+          } />
+          <Route path="/not-authorized" element={
+            <Layout showHeader={true}>
+              <NotAuthorized />
+            </Layout>
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,78 +1,62 @@
-// require("dotenv").config();
+require("dotenv").config();
 
-// const express = require("express");
-// const cookieParser = require("cookie-parser");
-// const authController = require("./controller/authController");
-// const applicationController = require('./controller/applicationController');
-// const app = express();
-// const cors = require("cors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authController = require("./controller/authController");
+const applicationController = require('./controller/applicationController');
+const app = express();
+const cors = require("cors");
 
-// app.use(express.json());
-// app.use(cookieParser());
+app.use(express.json());
+app.use(cookieParser());
 
-// // app.use(cors({
-// //   origin: 'http://localhost:3000', 
-// //   credentials: true, 
-// //   allowedHeaders: ['Content-Type', 'Authorization'],
-// // }));
-
-// // const corsOptions = {
-// //   origin: 'https://myapp-frontend-u9lq.onrender.com', // This will be the URL of your frontend app
-// //   credentials: true,
-// //   allowedHeaders: ['Content-Type', 'Authorization'],
-// // };
+// app.use(cors({
+//   origin: 'http://localhost:3000', 
+//   credentials: true, 
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
 
 // const corsOptions = {
-//   origin: process.env.CORS_ORIGIN,
+//   origin: 'https://myapp-frontend-u9lq.onrender.com', // This will be the URL of your frontend app
 //   credentials: true,
 //   allowedHeaders: ['Content-Type', 'Authorization'],
 // };
 
-// app.use(cors(corsOptions));
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 
 
-// // app.use(cors({ origin: '*', credentials: true }));
-// // app.use((req, res, next) => {
-// //   res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
-// //   res.header('Access-Control-Allow-Credentials', 'true');
-// //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-// //   if (req.method === 'OPTIONS') {
-// //     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-// //     return res.status(200).json({});
-// //   }
-// //   next();
-// // });
-
-
-// app.post("/login", authController.login);
-// app.post("/register", authController.register);
-// app.post("/apply", applicationController.submitApplication);
-// app.get("/apply", applicationController.handleCompetences);
-// app.get("/applications", applicationController.listAllApplications);
-// app.post("/applications", applicationController.setApplicationStatus);
-
-// const port = process.env.PORT || 5001;
-
-
-// const server = app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
+// app.use(cors({ origin: '*', credentials: true }));
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   if (req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+//     return res.status(200).json({});
+//   }
+//   next();
 // });
 
 
-// module.exports = { server };
+app.post("/login", authController.login);
+app.post("/register", authController.register);
+app.post("/apply", applicationController.submitApplication);
+app.get("/apply", applicationController.handleCompetences);
+app.get("/applications", applicationController.listAllApplications);
+app.post("/applications", applicationController.setApplicationStatus);
+
+const port = process.env.PORT || 5001;
 
 
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
-app.get('/', (req, res) => {
-  res.send({result: 'No CORS enabled here'});
-});
-app.get('/cors', (req, res) => {
-  res.set('Access-Control-Allow-Origin', 'https://myapp-frontend-u9lq.onrender.com');
-  res.send({result: 'This has CORS enabled'});
-});
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
+
+module.exports = { server };
